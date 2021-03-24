ed523df1e9e54d352a78d439c1c1da44e893ed79
Alexandre Gérault <alexandre@hexium.io>
2021-03-24 12:25:18 +0100

    refactor: remove json handling (json should be handled by api routes)

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -11,13 +11,11 @@ class Authenticate extends Middleware
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  Request  $request
-     * @return string
+     * @return string|void
      */
-    protected function redirectTo($request)
+    protected function redirectTo($request): string
     {
-        if (! $request->expectsJson()) {
-            session()->flash('error', 'Vous devez être connecté pour accèder à cette page');
-            return route('login');
-        }
+        session()->flash('error', 'Vous devez être connecté pour accéder à cette page');
+        return route('login');
     }
 }
